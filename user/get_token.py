5aa09e7ed7bea84297e2d04aabc5dc843c1b59ed
Victor-Krupeichenko <victor_krupeichenko@hotmail.com>
2023-08-31 13:45:46 +0300

    Добавлено: * Функция создания токена * Функция записи(сохранения) токена в cookies

---
+++ user/get_token.py
@@ -0,0 +1,30 @@
+from jose import jwt
+from setting_env import secret_key, algorithm, token_expiration_date
+from datetime import datetime, timedelta
+from fastapi import Response
+from setting_env import name_cookies
+
+
+def create_success_token(data: dict):
+    """
+    Создание токена
+    :param data: словарь с данными для кодирования токена
+    :return: закодированный токен
+    """
+
+    expire = datetime.now() + timedelta(days=int(token_expiration_date))  # срок действия токена
+    encode_jwt = jwt.encode({**data, "exp": expire}, key=secret_key, algorithm=algorithm)  # кодирование токена
+    return encode_jwt
+
+
+async def write_token_to_cookie(username, response: Response):
+    """
+    Записывает токен в cookies
+    :param username: имя пользователя
+    :param response: объект HTTP запроса
+    :return: токен
+    """
+
+    jwt_token = create_success_token(data={"username": username})
+    response.set_cookie(key=name_cookies, value=f"Bearer {jwt_token}", httponly=True)
+    return jwt_token
