2b6687741e719071bbd9556132221730977d7654
Victor-Krupeichenko <victor_krupeichenko@hotmail.com>
2023-09-02 13:02:17 +0300

    Добавлено: * Управление сессиями * Маршрут для папки со статическими файлами * Подключение endpoints вед приложения

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
 from fastapi import FastAPI
 from user.routers import user_router
 from weather.routers import weather_router
+from web_app.routers import web_router
+from fastapi.staticfiles import StaticFiles
+from starlette.middleware.sessions import SessionMiddleware
+from setting_env import secret_key_session
 
 app = FastAPI(
     title="Погода",
@@ -11,5 +15,8 @@ app = FastAPI(
         "email": "krupeichenkovictor@gmail.com"
     }
 )
+app.add_middleware(SessionMiddleware, secret_key=secret_key_session)  # управление сессиями
 app.include_router(user_router)
 app.include_router(weather_router)
+app.mount("/static", StaticFiles(directory="web_app/static"), name="static")
+app.include_router(web_router)
